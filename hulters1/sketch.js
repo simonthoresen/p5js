6d375f6d863c4f41bd8c37ea3a0d6b17d8088a0f
Simon Hult <simonhul@microsoft.com>
2018-04-02 16:39:44 +0200

    ADDED ASSETS

--- hulters1/sketch.js
+++ hulters1/sketch.js
@@ -1,7 +1,12 @@
-function setup() {
-  // put setup code here
-}
+var CANVAS_WIDTH = 800;
+var CANVAS_HEIGHT = 600;
 
-function draw() {
-  // put drawing code here
-}
+var canvas = document.createElement("canvas");
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+var c = canvas.getContext("2d");
+c.fillStyle = "green";
+c.fillRect(10, 10, 30, 30);
+
+this.document.body.appendChild(canvas);
