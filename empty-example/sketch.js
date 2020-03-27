82fd4304fffb63c92c0efa8095f6b8037280d1d2
Simon Hult <simonhul@microsoft.com>
2020-03-27 15:34:57 +0100

    add some examples to empty

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,8 +1,11 @@
 function setup() {
   // put setup code here
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   // put drawing code here
+  fill("LightSkyBlue");
+	rect(0, 0, windowWidth, windowHeight);
   ellipse(50, 50, 80, 80);
 }
